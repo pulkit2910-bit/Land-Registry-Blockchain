2dffe112570556cc9bb05d1bc79e33a7bbb83d00
pulkit2910-bit <ranjanpulkit2910@gmail.com>
2022-10-17 05:12:06 +0530

    deploy

---
+++ lib/constant/constants.dart
@@ -0,0 +1,17 @@
+String mapBoxApiKey =
+    'pk.eyJ1Ijoic2F1cmFiaG13IiwiYSI6ImNreTRiYzNidjBhMTkydnB2dmpoeGt4ZmgifQ.2QZ4CsNiygDTAhkqASpbPg';
+
+String nftStorageApiKey =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDJmNGUwQTQwNTI4MkMyMDNkZDBEZmY2NUNlMkUwRTYyQUNCODFDRWUiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTYzNzkwNzQxNjEwNSwibmFtZSI6ImxhbmRfZG9jdW1lbnQifQ.5ReEuIxsDhWxOLa2lVe9n-B2PUjdEkwJ5jLsBGdBDGA';
+
+const String rpcUrl = "https://goerli.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161";
+    
+    
+//"https://ropsten.infura.io/v3/e43345b7383246378963be7acd5b6c67";
+//"http://127.0.0.1:7545";
+
+const String contractAddress = "0x4b2b022f15faf1906f18d5270e7dcb0c751d0d93";
+
+//"0xCF6480c030219494B9033292B49f6d87af5768a1";
+
+const int chainId = 5; //Ropsten-3, Polygon testnet-80001, local ganache-1337, goerli - 5
